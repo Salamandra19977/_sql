ba30e0e82e6a750425b1019161c398e502fc1e51
Олексій Зубрицький <60927007+Salamandra19977@users.noreply.github.com>
2026-03-28 10:20:01 +0200

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO movies (
+    title,
+    release_date,
+    rating,
+    duration,
+    description,
+    filename,
+    poster_url
+)
+VALUES (
+    'The Shawshank Redemption',
+    '1994-10-14',
+    9.3,
+    142,
+    'Two imprisoned',
+    'video.mp3',
+    'poster.png'
+);
